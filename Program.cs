ef62274b8b1835b63ba44691d9920b13cb6c0eed
Reginald Williams Jr <rlwjr433@gmail.com>
2022-11-16 15:30:44 -0600

    Phase 7 completed! The Moose is full of personality!

--- Program.cs
+++ Program.cs
@@ -77,14 +77,73 @@ Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
 Console.WriteLine("--------------------------------------------");
 Console.WriteLine();
 
-bool isTrue = MooseAsks("Is... anything real?");
-// &phase 6 additions
+// bool isTrue = MooseAsks("Is... anything real?");
+// // &phase 6 additions
 
-if (isTrue)
+// if (isTrue)
+// {
+//     MooseSays("");
+// }
+// else
+// {
+//     MooseSays("I  K N E W  I T !!! *puts on tinfoil hat*");
+// }
+
+// & part 7 below 
+void CanadaQuestion()
 {
-    MooseSays("Really? That seems...unlikely. Where's the proof that we arent just projections of energy long past, like the way we see the light from the stars? where is the proof that time is even linear? YOU DON'T HAVE THESE ANSWERS, SWAY");
+    bool isTrue = MooseAsks("Is... anything real?");
+    if (isTrue)
+    {
+        MooseSays("Really? That seems...unlikely. Where's the proof that we arent just projections of energy long past, like the way we see the light from the stars? where is the proof that time is even linear? YOU DON'T HAVE THESE ANSWERS, SWAY");
+    }
+    else
+    {
+        MooseSays("I  K N E W  I T !!! *puts on tinfoil hat*");
+    }
 }
-else
+
+void EnthusiasticQuestion()
 {
-    MooseSays("I  K N E W  I T !!! *puts on tinfoil hat*");
+    bool isEnthusiastic = MooseAsks("Are you enthusiastic?");
+    if (isEnthusiastic)
+    {
+        MooseSays("Yay!");
+    }
+    else
+    {
+        MooseSays("You should try it!");
+    }
 }
+
+void LoveCSharpQuestion()
+{
+    bool doesLoveCSharp = MooseAsks("Do you love C# yet?");
+    if (doesLoveCSharp)
+    {
+        MooseSays("I mean... it seems fine!");
+    }
+    else
+    {
+        MooseSays("You will...oh, yes, you will...");
+    }
+}
+
+void SecretQuestion()
+{
+    bool wantsSecret = MooseAsks("Do you want to know a secret?");
+    if (wantsSecret)
+    {
+        MooseSays("I'm flattered that you think I'll keep it!");
+    }
+    else
+    {
+        MooseSays("That's probabaly not my business, is it?");
+    }
+}
+
+// Ask some questions
+CanadaQuestion();
+EnthusiasticQuestion();
+LoveCSharpQuestion();
+SecretQuestion();
