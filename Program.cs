d325dad25e879752a76926be36c7cfc60f216b47
Reginald Williams Jr <rlwjr433@gmail.com>
2022-11-16 14:52:16 -0600

    Phase 3 completed!

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 *todo: initialize project 
 *todo: delete boilerplate syntax
 *todo: update with alternative syuntax to display required result
+todo: let da moose get loose 
 */
 
 
@@ -9,3 +10,39 @@ using System;
 
 Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
 Console.WriteLine("--------------------------------------------");
+Console.WriteLine();
+
+
+void MooseSays()
+{
+    Console.WriteLine(@"
+                                       _.--^^^--,
+                                    .'          `\
+  .-^^^^^^-.                      .'              |
+ /          '.                   /            .-._/
+|             `.                |             |
+ \              \          .-._ |          _   \
+  `^^'-.         \_.-.     \   `          ( \__/
+        |             )     '=.       .,   \
+       /             (         \     /  \  /
+     /`               `\        |   /    `'
+     '..-`\        _.-. `\ _.__/   .=.
+          |  _    / \  '.-`    `-.'  /
+          \_/ |  |   './ _     _  \.'
+               '-'    | /       \ |
+                      |  .-. .-.  |
+                      \ / o| |o \ /
+                       |   / \   |    H I, I'M  E N T H U S I A S T I C !
+                      / `^`   `^` \
+                     /             \
+                    | '._.'         \
+                    |  /             |
+                     \ |             |
+                      ||    _    _   /
+                      /|\  (_\  /_) /
+                      \ \'._  ` '_.'
+                       `^^` `^^^`
+    ");
+}
+
+MooseSays();
